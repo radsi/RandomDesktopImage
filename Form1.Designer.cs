7dcc90880ba23677da35b1281e91ef72c5c85a18
radsi <78619513+radsi@users.noreply.github.com>
2023-10-22 02:23:24 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace RandomBackgroundImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(167, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 141);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set new image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(583, 270);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(353, 50);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Remember folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 451);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(982, 437);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(55)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(1006, 900);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Random Desktop Image";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+
+namespace RandomBackgroundImage
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        toolbox.Wallpaper.CustomWally customWally = new toolbox.Wallpaper.CustomWally();
+        Random random = new Random();
+
+        private string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".bmp", ".gif" };
+        private string folderPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetRandomDesktopBackground()
+        {
+            if(folderPath != "" && checkBox1.Checked)
+            {
+                GetAndSetImage(folderPath);
+                return;
+            }
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    if (checkBox1.Checked) folderPath = fbd.SelectedPath;
+                    GetAndSetImage(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void GetAndSetImage(string folderPath)
+        {
+            string[] allImageFiles = GetAllImageFiles(folderPath);
+
+            if (allImageFiles.Length > 0)
+            {
+                var image = allImageFiles[random.Next(allImageFiles.Length)];
+                textBox1.Text = image;
+                customWally.defaultBackgroundFile = image;
+                customWally.setWallforAllScreen(image);
+            }
+        }
+
+        private string[] GetAllImageFiles(string folderPath)
+        {
+            return Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories).Where(file => imageExtensions.Contains(Path.GetExtension(file).ToLower())).ToArray();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.FromArgb(44, 47, 51), ButtonBorderStyle.Solid);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetRandomDesktopBackground();
+        }
+    }
+}
+
+namespace toolbox.Wallpaper
+{
+    public class CustomWally
+    {
+        const int SetDeskWallpaper = 20;
+        const int UpdateIniFile = 0x01;
+        const int SendWinIniChange = 0x02;
+        public string defaultBackgroundFile;
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        Point primaryMonitorPoint = new Point(0, 0);
+        Dictionary<string, Image> images = new Dictionary<string, Image>();
+
+        public CustomWally()
+        {
+            //figure out where the main monitor is in relation to the virtualScreenBitmap
+            foreach (Screen scr in Screen.AllScreens)
+            {
+                images.Add(scr.DeviceName, null);
+                screenos.Add(scr.DeviceName);
+                if (scr.Bounds.Left < primaryMonitorPoint.X)
+                    primaryMonitorPoint.X = scr.Bounds.Left;
+                if (scr.Bounds.Top < primaryMonitorPoint.Y)
+                    primaryMonitorPoint.Y = scr.Bounds.Top;
+            }
+            primaryMonitorPoint.X *= -1;
+            primaryMonitorPoint.Y *= -1;
+
+            //Image for multiple screens
+            images.Add("all", null);
+
+            //set Images in Dictionary in case there are previous Images
+            if (File.Exists(defaultBackgroundFile))
+            {
+                using (var old = new Bitmap(defaultBackgroundFile))
+                {
+                    foreach (Screen scr in Screen.AllScreens)
+                    {
+                        Rectangle rectangle = new Rectangle(primaryMonitorPoint.X + scr.Bounds.Left, primaryMonitorPoint.Y + scr.Bounds.Top, scr.Bounds.Width, scr.Bounds.Height);
+                        if (old.Width >= (rectangle.X + rectangle.Width) &&
+                            old.Height >= (rectangle.Y + rectangle.Height))
+                            images[scr.DeviceName] = (Bitmap)old.Clone(rectangle, old.PixelFormat);
+                    }
+                }
+            }
+        }
+
+
+
+        List<string> screenos = new List<string>();
+        int index = 0;
+
+
+        public void setAlternatingWalls(string file)
+        {
+            images[screenos[index]] = Image.FromFile(file);
+            index++;
+            if (index == screenos.Count)
+                index = 0;
+
+            CreateBackgroundImage(Method.multiple, file);
+            GC.Collect();
+        }
+
+        public void setWallforScreen(Screen screen, string file)
+        {
+            images[screen.DeviceName] = Image.FromFile(file);
+            CreateBackgroundImage(Method.multiple, file);
+            GC.Collect();
+        }
+
+        public void setWallforAllScreen(string file)
+        {
+            images["all"] = Image.FromFile(file);
+            CreateBackgroundImage(Method.single, file);
+            GC.Collect();
+        }
+
+        private enum Method
+        {
+            multiple,
+            single
+        }
+        private void CreateBackgroundImage(Method method, string file)
+        {
+
+            using (var virtualScreenBitmap = new Bitmap((int)SystemInformation.VirtualScreen.Width, (int)System.Windows.Forms.SystemInformation.VirtualScreen.Height))
+            {
+                using (var virtualScreenGraphic = Graphics.FromImage(virtualScreenBitmap))
+                {
+
+                    switch (method)
+                    {
+                        // alternated Screen Images
+                        case Method.multiple:
+                            foreach (var screen in Screen.AllScreens)
+                            {
+                                // gets the image which we want to place in virtualScreenGraphic
+                                var image = (images.ContainsKey(screen.DeviceName)) ? images[screen.DeviceName] : null;
+
+                                //sets the position and size where the images will go
+                                Rectangle rectangle = new Rectangle(primaryMonitorPoint.X + screen.Bounds.Left, primaryMonitorPoint.Y + screen.Bounds.Top, screen.Bounds.Width, screen.Bounds.Height);
+
+                                // produce a image for the screen and fill it with the desired image... centered
+                                var monitorBitmap = new Bitmap(rectangle.Width, rectangle.Height);
+                                if (image != null)
+                                    DrawImageCentered(Graphics.FromImage(monitorBitmap), image, rectangle);
+
+                                //draws the picture at the right place in virtualScreenGraphic
+                                virtualScreenGraphic.DrawImage(monitorBitmap, rectangle);
+                            }
+                            break;
+
+                        //Single screen Image
+                        case Method.single:
+                            // gets the image which we want to place in virtualScreenGraphic
+                            var image2 = images["all"];
+
+                            //sets the position and size where the images will go
+                            Rectangle rectangle2 = new Rectangle(0, 0, virtualScreenBitmap.Width, virtualScreenBitmap.Height);
+
+                            // fill with the desired image... centered                            
+                            if (image2 != null)
+                                DrawImageCentered(virtualScreenGraphic, image2, rectangle2);
+
+                            //draws the picture at the right place in virtualScreenGraphic
+                            virtualScreenGraphic.DrawImage(virtualScreenBitmap, rectangle2);
+                            break;
+                    }
+                }
+            }
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 1.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+            SystemParametersInfo(SetDeskWallpaper, 0, file, UpdateIniFile | SendWinIniChange);
+        }
+
+
+        private void DrawImageCentered(Graphics g, Image img, Rectangle monitorRect)
+        {
+            double ratiodev = (1.0 * monitorRect.Width / monitorRect.Height) - (1.0 * img.Width / img.Height);
+            if (((1.0 * monitorRect.Width / monitorRect.Height > 1) && ratiodev > -0.25 && ratiodev < 0.25))
+            {
+                img = getsnappedIMG(img, monitorRect);
+            }
+
+
+            float heightRatio = (float)monitorRect.Height / (float)img.Height;
+            float widthRatio = (float)monitorRect.Width / (float)img.Width;
+            int height = monitorRect.Height;
+            int width = monitorRect.Width;
+            int x = 0;
+            int y = 0;
+
+            if (heightRatio < widthRatio)
+            {
+                width = (int)((float)img.Width * heightRatio);
+                height = (int)((float)img.Height * heightRatio);
+                x = (int)((float)(monitorRect.Width - width) / 2f);
+            }
+            else
+            {
+                width = (int)((float)img.Width * widthRatio);
+                height = (int)((float)img.Height * widthRatio);
+                y = (int)((float)(monitorRect.Height - height) / 2f);
+            }
+            Rectangle rect = new Rectangle(x, y, width, height);
+            g.DrawImage(img, rect);
+        }
+
+        private Image getsnappedIMG(Image img, Rectangle monitorRect)
+        {
+            double ratiodev = (1.0 * monitorRect.Width / monitorRect.Height) - (1.0 * img.Width / img.Height);
+            int height = img.Height;
+            int width = img.Width;
+
+            Rectangle rect;
+            if (ratiodev < 0)
+            {
+                rect = new Rectangle(0, 0, (int)((1.0 * monitorRect.Width / monitorRect.Height) * height), height);
+                rect.X = (width - rect.Width) / 2;
+            }
+            else
+            {
+                rect = new Rectangle(0, 0, width, (int)(1.0 * width / (1.0 * monitorRect.Width / monitorRect.Height)));
+                rect.Y = (height - rect.Height) / 2;
+            }
+
+
+            var img2 = (Bitmap)img;
+            return (Bitmap)img2.Clone(rect, img.PixelFormat);
+
+        }
+    }
+}
